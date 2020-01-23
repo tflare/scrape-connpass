0b812b5689b1ba40cabd9d2834c720471779528e
tflare <taki@tflare.com>
2020-01-23 20:24:21 +0900

    [fix]UseridRegExp, Dbクラスを分離 型がanyになっている箇所について修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,7 +12,6 @@ admin.initializeApp({
 
 export const attendance2db = functions.region('asia-northeast1').https.onRequest((request, response) => { // eslint-disable-line
 
-  getUserNamesAsync(admin);
+  getUserNamesAsync();
 
 })
-
