674e57830a696cec19d288d9b1db8f747f933bd7
tflare <taki@tflare.com>
2020-02-17 21:39:21 +0900

    [fix]collection('users')からuserIDを削除

--- functions/src/db.ts
+++ functions/src/db.ts
@@ -9,7 +9,6 @@ export class Db {
     const eventRef = db.collection('event').doc(String(eventID))
     eventRef.collection('users').doc(userID).set({
       eventID: eventID,
-      userID: userID,
       displayName: displayName,
       attendance: false,//出席フラグ今の段階ではfalseで登録
       presenter: presenter,
